609e9acae3ebaaf3247baa94eb8e788cf6eb27f2
caninereason <theciaranneeson@hotmail.com>
2023-03-28 03:52:40 +0100

    dasa

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'alphasecret44'
+SECRET_KEY = 'Alphasecret44'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
